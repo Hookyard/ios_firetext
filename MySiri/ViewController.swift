48eeed6d7fbe606acc37c18c44b78973b0ee1307
Edgar, Tristan, Jules <strippolijules@gmail.com>
2021-12-01 09:19:21 +0100

    Initial Commit

---
+++ MySiri/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySiri
+//
+//  Created by Tristan Sarrazin on 01/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
